492b8064a4c5ade5ef9634da0169fd6861a5ae1d
phroxzon <mlxtreme@gmail.com>
2018-12-22 21:03:22 -0800

    Unignoring build file

---
+++ build/precache-manifest.abc0999a8dfea2973b4590936d3a5995.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b0de201172da37e154730a85ee8c903",
+    "url": "/static/media/BebasNeueLight.8b0de201.ttf"
+  },
+  {
+    "revision": "59af18df009474c5a6a9",
+    "url": "/static/css/main.faba4e82.chunk.css"
+  },
+  {
+    "revision": "c77a3669e27a298011af",
+    "url": "/static/js/1.c77a3669.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "42635767293f082644e05307170e766f",
+    "url": "/static/media/up.42635767.svg"
+  },
+  {
+    "revision": "63a246b46f9d3afce322ec36d4ff90df",
+    "url": "/static/media/down.63a246b4.svg"
+  },
+  {
+    "revision": "e657ceda82505098040fe1080709ba6f",
+    "url": "/static/media/nochange.e657ceda.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "13ecc94ba371c5b929d414f90704d737",
+    "url": "/static/media/akali.13ecc94b.png"
+  },
+  {
+    "revision": "0fb876af87eea8b6e1577f9609fa1a6f",
+    "url": "/static/media/Ionia_Placidium_Navori.0fb876af.png"
+  },
+  {
+    "revision": "1de03e44f3bd7689ce4dd8ae2625ad63",
+    "url": "/static/media/video.1de03e44.mp4"
+  },
+  {
+    "revision": "148f4a914dae3cbdabdb2413307daadd",
+    "url": "/static/media/BebasNeue-Thin.148f4a91.eot"
+  },
+  {
+    "revision": "c4bb0b733c94cebfd814984542512b2a",
+    "url": "/static/media/BebasNeue-Thin.c4bb0b73.ttf"
+  },
+  {
+    "revision": "db2f06f770c5ceb2b0beec967f72be4d",
+    "url": "/static/media/BebasNeue-Thin.db2f06f7.woff"
+  },
+  {
+    "revision": "a6f54115b0398cd2acb4da75ccfd6cad",
+    "url": "/static/media/BebasNeueBook.a6f54115.eot"
+  },
+  {
+    "revision": "1b90eff826d35da2eecc7f92768f2ca0",
+    "url": "/static/media/BebasNeueBook.1b90eff8.woff"
+  },
+  {
+    "revision": "a3ae5c633b3d0d63a41d57376ce68417",
+    "url": "/static/media/BebasNeueBook.a3ae5c63.ttf"
+  },
+  {
+    "revision": "4a8b2f03140c5031b34d9c6dca9a0ca6",
+    "url": "/static/media/BebasNeueBold.4a8b2f03.ttf"
+  },
+  {
+    "revision": "c955e50d75cbc866cf241a0a8a63a9e1",
+    "url": "/static/media/BebasNeueRegular.c955e50d.woff"
+  },
+  {
+    "revision": "b397d82133e1d66cf7a112d76e58f0f7",
+    "url": "/static/media/BebasNeueBold.b397d821.eot"
+  },
+  {
+    "revision": "10b33c35839aacea36d00bcf578f5917",
+    "url": "/static/media/BebasNeueRegular.10b33c35.ttf"
+  },
+  {
+    "revision": "9772e40ea6adf906cc279a5ad9ea2386",
+    "url": "/static/media/BebasNeueLight.9772e40e.woff"
+  },
+  {
+    "revision": "6c8e7e55339b8aa36f8629ec9089bdfe",
+    "url": "/static/media/BebasNeueLight.6c8e7e55.eot"
+  },
+  {
+    "revision": "2f85805387350e2a954f2b106670a85e",
+    "url": "/static/media/HelveticaNeueLight.2f858053.eot"
+  },
+  {
+    "revision": "a44f731df8d077f3c3a9b83e05ab800b",
+    "url": "/static/media/HelveticaNeueLight.a44f731d.woff"
+  },
+  {
+    "revision": "59af18df009474c5a6a9",
+    "url": "/static/js/main.59af18df.chunk.js"
+  },
+  {
+    "revision": "f4160341f66d20ec7fe6ac1712203f51",
+    "url": "/static/media/HelveticaNeueBlackCond.f4160341.woff"
+  },
+  {
+    "revision": "00f8d8ab4d3495f7c3ea6bca6f9f9e16",
+    "url": "/static/media/HelveticaNeueBlackCond.00f8d8ab.eot"
+  },
+  {
+    "revision": "f7a4f1f0584d73eb3bd8b078f4ef4931",
+    "url": "/static/media/HelveticaNeueLight.f7a4f1f0.ttf"
+  },
+  {
+    "revision": "e2004f3b409ae9a97f4c6fde34447a16",
+    "url": "/static/media/HelveticaNeueBlackCond.e2004f3b.ttf"
+  },
+  {
+    "revision": "92f689c73c8c32164fb5a80129f73747",
+    "url": "/static/media/HelveticaNeueBold.92f689c7.eot"
+  },
+  {
+    "revision": "02183900bb0957e0e50fcbf3b713dcd9",
+    "url": "/static/media/HelveticaNeueBold.02183900.woff"
+  },
+  {
+    "revision": "e1b7f8b132b5f773d28151a051a4badb",
+    "url": "/static/media/HelveticaNeueBold.e1b7f8b1.ttf"
+  },
+  {
+    "revision": "8ef0465bad53ce75d7caabcb4e609eb5",
+    "url": "/static/media/HelveticaNeueMedium.8ef0465b.ttf"
+  },
+  {
+    "revision": "83d895e9e747d713932c019280e1dd0f",
+    "url": "/static/media/HelveticaNeueMedium.83d895e9.woff"
+  },
+  {
+    "revision": "b8f8efdca0f3ae907f218ba6044d413d",
+    "url": "/static/media/HelveticaNeueThin.b8f8efdc.woff"
+  },
+  {
+    "revision": "f512e40ea8371ed38c93cb941ace3399",
+    "url": "/static/media/HelveticaNeueMedium.f512e40e.eot"
+  },
+  {
+    "revision": "88ac35d1208c3cc4b1dc446a4096250c",
+    "url": "/static/media/HelveticaNeueThin.88ac35d1.eot"
+  },
+  {
+    "revision": "7452c0377ca3a6566b2953c9273fd092",
+    "url": "/static/media/HelveticaNeueHeavy.7452c037.woff"
+  },
+  {
+    "revision": "b10eb845ba1b1bd0a56f459daac28da0",
+    "url": "/static/media/HelveticaNeueThin.b10eb845.ttf"
+  },
+  {
+    "revision": "fa19970a02a98b9f63d93841986b8573",
+    "url": "/static/media/HelveticaNeueHeavy.fa19970a.ttf"
+  },
+  {
+    "revision": "ff064da719d8565cc035648ec454b90c",
+    "url": "/static/media/HelveticaNeueHeavy.ff064da7.eot"
+  },
+  {
+    "revision": "ccac99018064e8f0e9b02f5cee43a40e",
+    "url": "/static/media/HelveticaNeueRegular.ccac9901.ttf"
+  },
+  {
+    "revision": "79d4c45ced0f96d39ebea465d66a5052",
+    "url": "/static/media/HelveticaNeueRegular.79d4c45c.eot"
+  },
+  {
+    "revision": "e92c2435a055a23901d721d60b5c3a8b",
+    "url": "/static/media/HelveticaNeueRegular.e92c2435.woff"
+  },
+  {
+    "revision": "012fd253216baec97af8df696cbc5430",
+    "url": "/static/media/HelveticaNeueItalic.012fd253.woff"
+  },
+  {
+    "revision": "5f9686945265154e1087dad981d7cc26",
+    "url": "/static/media/HelveticaNeueItalic.5f968694.eot"
+  },
+  {
+    "revision": "be1fb8c29b6e338a31af8b37c1e6b3d6",
+    "url": "/static/media/HelveticaNeueItalic.be1fb8c2.ttf"
+  },
+  {
+    "revision": "65f1c5f98b8aaf929c84e16c40b08e02",
+    "url": "/static/media/BebasNeueBold.65f1c5f9.woff"
+  },
+  {
+    "revision": "bad9ee55d378044d181d920d34259d9c",
+    "url": "/static/media/BebasNeueRegular.bad9ee55.eot"
+  },
+  {
+    "revision": "ad955a29d925a07c427978729ec0ae4a",
+    "url": "/index.html"
+  }
+];
